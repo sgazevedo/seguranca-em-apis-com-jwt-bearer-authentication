bad78ba7f39baf29a1c1dd80cdaf40eb9029588c
sgazevedo <sgazevedo@gmail.com>
2023-10-24 15:04:10 -0300

    feat(): criada classe de extensao para tratar obter props de claimsprincipal

--- src/JwtAspNet/Program.cs
+++ src/JwtAspNet/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+using JwtAspNet.Extensions;
 using JwtAspNet.IoC;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
@@ -13,7 +15,15 @@ app.UseAuthorization();
 
 app.MapGet("/login", (TokenService tokenService, [FromBody] User user) => tokenService.Create(user));
 
-app.MapGet("/restrito", () => "Você tem acesso!").RequireAuthorization();
+app.MapGet("/restrito", (ClaimsPrincipal user) => 
+  new {
+    id = user.Id(),
+    name = user.Name(),
+    email = user.Email(),
+    givenName = user.GivenName(),
+    image = user.Image()
+  }
+).RequireAuthorization();
 
 app.MapGet("/admin", () => "Você tem acesso!").RequireAuthorization("admin");
 
