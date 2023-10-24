c6a69f290ae196048dd70ad52124e02fecb6a378
sgazevedo <sgazevedo@gmail.com>
2023-10-24 16:36:00 -0300

    feat(): adicionadas configuracoes iniciais da app

--- src/JwtStore/JwtStore.Api/Program.cs
+++ src/JwtStore/JwtStore.Api/Program.cs
@@ -1,4 +1,10 @@
+using JwtStore.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddConfiguration();
+builder.AddDatabase();
+builder.AddJwtAuthentication();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
--- src/JwtStore/JwtStore.Core/Configuration.cs
+++ src/JwtStore/JwtStore.Core/Configuration.cs
@@ -2,7 +2,13 @@ namespace JwtStore.Core
 {
   public static class Configuration
   {
-    public static SecretsConfiguration Secrets { get; set; } = new();    
+    public static DatabaseConfiguration Database { get; set; } = new();
+    public static SecretsConfiguration Secrets { get; set; } = new();
+
+    public class DatabaseConfiguration
+    {
+      public string ConnectionString { get; set; } = string.Empty;
+    }
 
     public class SecretsConfiguration
     {
