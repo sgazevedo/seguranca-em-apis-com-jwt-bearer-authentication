417671485e7eb66b9c9c0ae2a7d54b575021b8e5
sgazevedo <sgazevedo@gmail.com>
2023-10-24 14:05:20 -0300

    feat(): configurado autenticacao

--- src/JwtAspNet/IoC/DependencyInjection.cs
+++ src/JwtAspNet/IoC/DependencyInjection.cs
@@ -1,4 +1,7 @@
+using System.Text;
 using JwtAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAspNet.IoC
 {
@@ -12,7 +15,23 @@ namespace JwtAspNet.IoC
 
     public static void AddSecurity(this IServiceCollection services)
     {
-      services.AddAuthentication();
+      services.AddAuthentication(x =>
+      {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      })
+      .AddJwtBearer(x =>
+      {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+          ValidateIssuer = false,
+          ValidateAudience = false
+        };
+      });
+      
       services.AddAuthorization();
     }
 
