ee22d58ce4bff62c7080ebe8a57995fb73422ecb
sgazevedo <sgazevedo@gmail.com>
2023-10-25 16:24:38 -0300

    feat(): implementado handler de authenticate

---
+++ src/JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Authenticate/Contracts/IRepository.cs
@@ -0,0 +1,9 @@
+using JwtStore.Core.Contexts.AccountContext.Entities;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Contracts
+{
+  public interface IRepository
+  {
+    Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken);
+  }
+}
---
+++ src/JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Authenticate/Request.cs
@@ -0,0 +1,6 @@
+using MediatR;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate
+{
+  public record Request(string Email, string Password) : IRequest<Response>;
+}
