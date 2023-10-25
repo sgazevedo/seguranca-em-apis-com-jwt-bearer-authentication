244ac726964309a702cd77cca180c6f11cd636d9
sgazevedo <sgazevedo@gmail.com>
2023-10-25 11:12:16 -0300

    feat(): implementado resposta generica e de accountcontext

---
+++ src/JwtStore/JwtStore.Core/Contexts/AccountContext/UseCases/Create/Response.cs
@@ -0,0 +1,30 @@
+using Flunt.Notifications;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create
+{
+  public class Response : ShareContext.UseCases.Response
+  {
+    protected Response()
+    {
+    }
+
+    public Response(string message, int status, IEnumerable<Notification>? notifications = null)
+    {
+      Message = message;
+      Status = status;
+      Notifications = notifications;
+    }
+
+    public Response(string message, ResponseData data)
+    {
+      Message = message;
+      Status = 201;
+      Notifications = null;
+      Data = data;
+    }
+
+    public ResponseData? Data { get; set; }
+  }
+
+  public record ResponseData(Guid Id, string Name, string Email);
+}
---
+++ src/JwtStore/JwtStore.Core/Contexts/ShareContext/UseCases/Response.cs
@@ -0,0 +1,12 @@
+using Flunt.Notifications;
+
+namespace JwtStore.Core.Contexts.ShareContext.UseCases
+{
+  public abstract class Response
+  {
+    public string Message { get; set; } = string.Empty;
+    public int Status { get; set; } = 400;
+    public bool IsSuccess => Status is >= 200 and <= 299;
+    public IEnumerable<Notification>? Notifications { get; set; }
+  }
+}
